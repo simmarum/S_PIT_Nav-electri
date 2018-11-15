4e426fc31b374c8acaee45844ceb37fa9819ba7d
simmarum <mateusz0831@gmail.com>
2018-11-15 17:59:28 +0100

    Initial commit with simple swipe/tab navigation bar

---
+++ app/src/main/java/com/example/nav/nav_electri/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.nav.nav_electri;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.navigation_icon));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.stations_icon));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.attractions_icon));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.settings_icon));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
---
+++ app/src/main/java/com/example/nav/nav_electri/PagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.nav.nav_electri;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                TabNavigation tabNavigation = new TabNavigation();
+                return tabNavigation;
+            case 1:
+                TabStations tabStations = new TabStations();
+                return tabStations;
+            case 2:
+                TabAttractions tabAttractions = new TabAttractions();
+                return tabAttractions;
+            case 3:
+                TabSettings tabSettings  = new TabSettings();
+                return tabSettings;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
